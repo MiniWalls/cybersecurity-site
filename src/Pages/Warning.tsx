9641775dc5ce47d6c14ff5926f1be9bb05553be0
Santtu P <69449726+MiniWalls@users.noreply.github.com>
2023-11-01 14:35:31 +0900

    Now possible to send dangerous http requests and copy button for example requests

--- src/Pages/Warning.tsx
+++ src/Pages/Warning.tsx
@@ -1,11 +1,28 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import ClipboardCopy from "../components/ClipboardCopy";
 
 const Warning = (): JSX.Element => {
   const [queryParam, setQueryParam] = useState<string>("");
+  const [data, setData] = useState<string[]>([]);
 
   const submitClick = () => {
+    setData([]);
     const url = "http://localhost:3001/products/vulnerable?name=" + encodeURIComponent(queryParam);
     console.log(url);
+
+    axios.get(url)
+    .then((res: { data: { name: string }[] }) => {
+      console.log(res);
+      setData(prevData => {
+        if (Array.isArray(prevData)) {
+          return [...prevData, ...res.data.map((item) => item?.name ?? '')];
+        }
+        return [] as string[];
+      });
+    }).catch((err: Error) => {
+      console.log(err);
+    });
   };
 
 
@@ -20,15 +37,20 @@ const Warning = (): JSX.Element => {
     <>
       <h1 className="text-red-600 text-2xl">SQL Injection playground!!!</h1>
       <form className="mt-2" >
-        <input type="text" className="border-2 border-red-100 p-1" value={queryParam} onChange={(e) => setQueryParam(e.target.value)}/>
-        <button className="border-2 border-red-100 p-1 mx-2" type="button" onClick={() => submitClick()}>press</button>
+        <input type="text" className="border-2 border-red-100 p-1 rounded-md" value={queryParam} onChange={(e) => setQueryParam(e.target.value)}/>
+        <button className="border-2 border-red-100 p-1 mx-2 rounded-md" type="button" onClick={() => submitClick()}>press</button>
         <div className="bg-gray-200 max-w-fit p-2 mt-2">
-        58' OR 1 = 1; #<br/>
-        lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<br/>
+        <ClipboardCopy copyText="apple" />
+        <ClipboardCopy copyText="apple' OR 1 = 1; #" />
         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<br/>
         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<br/>
         </div>
       </form>
+      {data.map((item) => (
+        <div className="bg-gray-200 max-w-fit p-2 mt-2">
+          <p>{item}</p>
+        </div>
+      ))}
     </>
   );
 };
---
+++ src/components/ClipboardCopy.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+interface ComponentProps {
+  copyText: string;
+}
+
+function ClipboardCopy({ copyText }: ComponentProps): JSX.Element {
+  const [isCopied, setIsCopied] = useState(false);
+
+  async function copyTextToClipboard(text: string) {
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(text);
+    } else {
+      return document.execCommand('copy', true, text); //Deprecated but there's no alternative made for it.
+    }
+  }
+
+  const handleCopyClick = () => {
+    // Asynchronously call copyTextToClipboard
+    copyTextToClipboard(copyText)
+      .then(() => {
+        // If successful, update the isCopied state value
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 900);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <input type="text" value={copyText} readOnly className="mx-1 my-1 pl-1 rounded-md border-gray-300 border-[1px]"/>
+      <button type="button" onClick={handleCopyClick} className="px-1 rounded-md border-gray-800 border-[1px]">
+        <span>{isCopied ? 'Copied!' : 'Copy'}</span>
+      </button>
+    </div>
+  );
+}
+
+export default ClipboardCopy;
